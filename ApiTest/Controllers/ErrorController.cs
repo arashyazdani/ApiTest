93c616e718339846c1e00cb474e9754b04e105e5
Arash Yazdani <arash.yazdani.b@gmail.com>
2020-08-16 21:00:09 +0430

    agasdg

--- ApiTest/Controllers/ErrorController.cs
+++ ApiTest/Controllers/ErrorController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ApiTest.Errors;
+﻿using ApiTest.Errors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiTest.Controllers
