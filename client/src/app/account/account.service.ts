cd8355864368d03abf0de22b4927d6a9388d8a31
Arash Yazdani <arash.yazdani.b@gmail.com>
2020-08-31 00:53:59 +0430

    End of  Section 17

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, ReplaySubject, of } from 'rxjs';
 import { IUser } from '../shared/models/user';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
@@ -12,20 +12,19 @@ import { Router } from '@angular/router';
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
-  private currentUserSource = new BehaviorSubject<IUser>(null);
+  private currentUserSource = new ReplaySubject<IUser>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  getCurrentUserValue() {
-    return this.currentUserSource.value;
-  }
-
   loadCurrentUser(token: string) {
+    if (token === null) {
+      this.currentUserSource.next(null);
+      return of(null);
+    }
+
     let headers = new HttpHeaders();
 
-    //headers = headers.set('Authorization', 'Bearer ${token}');
-    //headers = headers.set('Authorization', String.Format('Bearer {token}', token));
     headers = headers.set('Authorization', 'Bearer ' + token);
 
     return this.http.get(this.baseUrl + 'account', { headers }).pipe(map((user: IUser) => {
